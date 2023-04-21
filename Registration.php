9a82e2eb59ee6c0c87c417d201f0008018dd137c
Alex Phoummalayvane <aphoumma@student.fitchburgstate.edu>
2023-04-20 21:35:15 -0400

    Add required fields for customer registration form
    - customer needs to fill in all required fields in order to register with bank. 
    - name, username, phone number, email, government ID Num, and Password

--- Registration.php
+++ Registration.php
@@ -6,21 +6,23 @@
     <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
+    
+    
 <?php
     require('dbconnect.php');
-    // When form submitted, insert values into the database.
-    if (isset($_REQUEST['username'])) {
+    // When form is submit, INSERT values into the database.
+    if (isset($_REQUEST['User'])) {
         // removes backslashes
-        $username = stripslashes($_REQUEST['username']);
+        $username = stripslashes($_REQUEST['User']);
         //escapes special characters in a string
         $username = mysqli_real_escape_string($con, $username);
-        $email    = stripslashes($_REQUEST['email']);
+        $email    = stripslashes($_REQUEST['Email']);
         $email    = mysqli_real_escape_string($con, $email);
-        $password = stripslashes($_REQUEST['password']);
+        $password = stripslashes($_REQUEST['Pass']);
         $password = mysqli_real_escape_string($con, $password);
-        $create_datetime = date("Y-m-d H:i:s");
-        $query    = "INSERT into `users` (username, password, email, create_datetime)
-                     VALUES ('$username', '" . md5($password) . "', '$email', '$create_datetime')";
+   
+        $query    = "INSERT into `customer` (User, Pass, Email)
+                     VALUES ('$username', '" . md5($password) . "', '$email')";
         $result   = mysqli_query($con, $query);
         if ($result) {
             echo "<div class='form'>
@@ -37,11 +39,15 @@
 ?>
     <form class="form" action="" method="post">
         <h1 class="login-title">Registration</h1>
-        <input type="text" class="login-input" name="username" placeholder="Username" required />
-        <input type="text" class="login-input" name="email" placeholder="Email Adress">
-        <input type="password" class="login-input" name="password" placeholder="Password">
+        <input type="text" class="login-input" name="name" placeholder="Name" required />
+        <input type="text" class="login-input" name="user" placeholder="Username" required />
+        <input type="text" class="login-input" name="phone" placeholder="Phone Number" required />
+        <input type="text" class="login-input" name="address" placeholder="Address" required />
+        <input type="text" class="login-input" name="email" placeholder="Email Address" required />
+        <input type="text" class="login-input" name="GovIDNum" placeholder="Government ID Number" required />
+        <input type="password" class="login-input" name="password" placeholder="Password" required />
         <input type="submit" name="submit" value="Register" class="login-button">
-        <p class="link"><a href="login.php">Click to Login</a></p>
+        <p class="link"><a href="CustomerSignIn.php">Click to Login</a></p>
     </form>
 <?php
     }
