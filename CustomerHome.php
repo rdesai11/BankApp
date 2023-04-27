d195ebf5eb1c9d52ba16b43dff6475f6882bc175
Alex Phoummalayvane <aphoumma@student.fitchburgstate.edu>
2023-04-26 21:58:31 -0400

    Create logout button for customer
    - used code from BankLogoutAction to handle the logout because it was reusable

--- CustomerHome.php
+++ CustomerHome.php
@@ -14,7 +14,7 @@ session_start();
     <div class="form">
         <p>Hey, <?php echo $_SESSION['User']; ?>!</p>
         <p>You are now in customer dashboard page.</p>
-        <p><a href="logout.php">Logout</a></p>
+        <p><a href="BankLogoutAction.php">Logout</a></p>
     </div>
 </body>
 </html>
